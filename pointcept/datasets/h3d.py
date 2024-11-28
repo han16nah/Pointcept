ad5d659085303dd7bf2e75e146be3cf410015d39
han16nah <han_nah@mailbox.org>
2024-11-29 00:08:16 +0100

    rename dataset

--- pointcept/datasets/h3d.py
+++ pointcept/datasets/h3d.py
@@ -10,7 +10,7 @@ from .builder import DATASETS
 
 
 @DATASETS.register_module()
-class H3DDataset(DefaultDataset):
+class Hessigheim3DDataset(DefaultDataset):
     def get_data_name(self, idx):
         # data_list contains all individual file paths - we want the folder name
         area_name = Path(self.data_list[idx]).name
