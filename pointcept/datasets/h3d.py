fc966c916120adc4090c9e8dd91e5116c389c5a1
han16nah <han_nah@mailbox.org>
2024-12-25 11:44:15 +0100

    debugging

--- pointcept/datasets/h3d.py
+++ pointcept/datasets/h3d.py
@@ -27,5 +27,5 @@ class Hessigheim3DDataset(DefaultDataset):
     
     def get_data_name(self, idx):
         # data_list contains all individual file paths - we want the folder name
-        area_name = Path(self.data_list[idx]).name
+        area_name = Path(self.data_list[idx % len(self.data_list)]).name
         return f"{area_name}"
